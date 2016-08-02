27146e3bc1ff98e312abe719a3cd06371cf33f6b
Alice Sady <asady1@jhu.edu>
2016-08-02 13:51:28 -0500

    adding plotting code

---
+++ runttree_vh_plots.sh
@@ -0,0 +1,7 @@
+python ttreeAnalyzer_76X_vh_alphabet_plots.py QCD/QCD_550_vh.root --outName=tree_QCD_500_VH_alphPlots.root --ntevt=1.0 &
+python ttreeAnalyzer_76X_vh_alphabet_plots.py QCD/QCD_700_vh.root --outName=tree_QCD_700_VH_alphPlots.root --ntevt=1.0 &
+python ttreeAnalyzer_76X_vh_alphabet_plots.py QCD/QCD_1000_vh.root --outName=tree_QCD_1000_VH_alphPlots.root --ntevt=1.0 &
+python ttreeAnalyzer_76X_vh_alphabet_plots.py QCD/QCD_1500_vh.root --outName=tree_QCD_1500_VH_alphPlots.root --ntevt=1.0 &
+python ttreeAnalyzer_76X_vh_alphabet_plots.py QCD/QCD_2000_vh.root --outName=tree_QCD_2000_VH_alphPlots.root --ntevt=1.0 &
+
+echo "done"
